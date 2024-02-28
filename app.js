ce2c1fea38a6c31649da453f7f1cce964dc1f66e
waruimoojin <waruimoojin>
2024-02-28 19:19:20 +0100

    Commit collection file and config

--- app.js
+++ app.js
@@ -1,18 +1,49 @@
-const mongoose = require('mongoose')
+const mongoose = require('mongoose');
+const express = require('express');
+const users = require('./models/usersModel');
+const app = express();
+const port = 3000;
 
-
-const connectDB = async() => {
+// DB CONNECTION
+const connectDB = async () => {
     try {
-        const connect = await mongoose.connect("mongodb://172.20.0.2:27017/DB", {
-        useNewUrlParser: true,
-        useUnifiedTopology: true
-        })
-        console.log("MongoDB Connected")
+        await mongoose.connect("mongodb://admin:pass@localhost:3000/BudgetDB", {
+            ///useNewUrlParser: true,
+            // Remove useUnifiedTopology option if using MongoDB Node.js driver 4.0.0 or newer
+            // useUnifiedTopology: true
+            serverSelectionTimeoutMS: 30000, 
+    socketTimeoutMS: 45000,
+        });
+        console.log("MongoDB Connected");
+    } catch (err) {
+        console.error(err);
+        process.exit(1);
     }
-    catch(err){
-        console.error(err)
-        process.exit(1)
-    }
-}
+};
 
-connectDB()
+connectDB();
+
+
+
+
+// POST
+app.use(express.json());
+
+app.post('/users' ,async(req,res)=>{
+
+   try {
+    const Users = await users.create(req.body)
+    res.status(200).json(Product)
+   
+   } catch (error) {
+    console.log(error.message);
+    res.status(500).json({message : error.message})
+    
+   }
+
+
+})
+
+app.listen(port, () => {
+    console.log(`Example app listening on port 3000`);
+});
---
+++ models/usersModel.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose');
+
+
+const usersSch = {
+    name: {
+        type: String,
+        required : [true , "Please enter your name"]
+
+    },
+    id: {
+        type: Number,
+
+    }
+
+}
+
+const users = mongoose.model('users', usersSch);
+
+module.exports = users;
