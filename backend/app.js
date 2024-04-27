22d8767042e2a32158e7e4a44fc8e79d93f7c7e7
mrbaloch555 <mehello456@gmail.com>
2024-04-28 04:46:22 +0500

    setup testing

--- backend/app.js
+++ backend/app.js
@@ -1,12 +1,22 @@
-const mongoose = require('mongoose');
-const express = require('express');
-const jwt = require('jsonwebtoken'); // Import JWT library
-const users = require('./models/usersModel');
+const mongoose = require("mongoose");
+const express = require("express");
+const jwt = require("jsonwebtoken"); // Import JWT library
+const users = require("./models/usersModel");
 const cors = require("cors");
 const app = express();
 const port = 3000;
-const bcrypt = require('bcrypt');
-const { authRoutes, budgetRoutes, transactionRoutes, categoryRoutes  } = require("./routes")
+const bcrypt = require("bcrypt");
+const {
+  authRoutes,
+  budgetRoutes,
+  transactionRoutes,
+  categoryRoutes,
+} = require("./routes");
+
+//** IMPORTANT */
+process.env.ENV = "test"; // use .env and set there in .env just put ENV = "production"
+// this is dependent below connection code
+
 const { errorConverter, errorHandler } = require("./middlewares/errorHandler");
 // DB CONNECTION
 async function connectDB() {
@@ -18,43 +28,38 @@ async function connectDB() {
     process.exit(1);
   }
 }
+if (process.env.ENV !== "test") connectDB();
 
-connectDB();
-
-
-app.use(express.json({}))
+app.use(express.json({}));
 app.use(cors());
 
-
 app.use((_req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'http://localhost:3001');
-  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization'); // Include Authorization header
+  res.header("Access-Control-Allow-Origin", "http://localhost:3001");
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization"); // Include Authorization header
   next();
 });
-app.options('*', (_req, res) => {
-  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization'); // Include Authorization header
+app.options("*", (_req, res) => {
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization"); // Include Authorization header
   res.sendStatus(200);
 });
 
 // routes
-app.use("/api/", authRoutes)
-app.use("/budget", budgetRoutes)
-app.use("/transaction", transactionRoutes)
-app.use("/category" , categoryRoutes)
-
-
+app.use("/api/", authRoutes);
+app.use("/budget", budgetRoutes);
+app.use("/transaction", transactionRoutes);
+app.use("/category", categoryRoutes);
 
 // POST route for user registration
-app.post('/users', async (req, res) => {
+app.post("/users", async (req, res) => {
   try {
     const { email, password } = req.body;
 
     // Vérifier si l'utilisateur existe déjà dans la base de données
     const existingUser = await users.findOne({ email });
     if (existingUser) {
-      return res.status(400).json({ message: 'User already exists' });
+      return res.status(400).json({ message: "User already exists" });
     }
 
     // Hacher le mot de passe avant de l'enregistrer dans la base de données
@@ -64,17 +69,20 @@ app.post('/users', async (req, res) => {
     const newUser = await users.create({ email, password: hashedPassword });
 
     // Générer le jeton JWT pour le nouvel utilisateur
-    const token = jwt.sign({ userId: newUser._id, email: newUser.email }, 'your_secret_key');
+    const token = jwt.sign(
+      { userId: newUser._id, email: newUser.email },
+      "your_secret_key"
+    );
 
     // Envoyer le jeton JWT en réponse
     res.status(201).json({ token });
   } catch (error) {
-    console.error('Registration failed:', error);
-    res.status(500).json({ message: 'Internal server error' });
+    console.error("Registration failed:", error);
+    res.status(500).json({ message: "Internal server error" });
   }
 });
 // GET
-app.get('/users', async (req, res) => {
+app.get("/users", async (req, res) => {
   try {
     const allUsers = await users.find();
     res.status(200).json(allUsers);
@@ -95,7 +103,8 @@ app.use(errorConverter);
 // handle error
 app.use(errorHandler);
 
-app.listen(port, () => {
-  console.log(`Example app listening on port 3000`);
-});
+// app.listen(port, () => {
+//   console.log(`Example app listening on port 3000`);
+// });
 
+module.exports = app;
